a58faefd3696242fcff74b15e23c713d4b3f38cf
Philipp Heuer <git@philippheuer.me>
2022-05-01 16:21:40 +0200

    docs: comments in openpgp.go

--- pkg/core/secret/openpgp.go
+++ pkg/core/secret/openpgp.go
@@ -1,16 +1,18 @@
 package secret
 
+// see: https://github.com/ProtonMail/gopenpgp
+
 import (
 	"github.com/ProtonMail/gopenpgp/v2/helper"
 )
 
-// EncryptOpenPGP encrypts a secretString with the publicKey
+// EncryptOpenPGP encrypts a secretString with the publicKey using OpenPGP
 func EncryptOpenPGP(publicKey string, secretString string) (string, error) {
 	// encrypt plain text message using public key
 	return helper.EncryptMessageArmored(publicKey, secretString)
 }
 
-// DecryptOpenPGP decrypts a encrypted string
+// DecryptOpenPGP decrypts an encrypted string using OpenPGP
 func DecryptOpenPGP(privateKey string, privateKeyPassword string, encString string) (string, error) {
 	// decrypt armored encrypted message using the private key and obtain plain text
 	return helper.DecryptMessageArmored(privateKey, []byte(privateKeyPassword), encString)
