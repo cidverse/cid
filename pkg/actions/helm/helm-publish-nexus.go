9135c440a91bdc2abab5aeb9fc3c6f15e427e67d
Philipp Heuer <git@philippheuer.me>
2022-07-05 23:21:34 +0200

    chore: formatting in helm-publish-nexus

--- pkg/actions/helm/helm-publish-nexus.go
+++ pkg/actions/helm/helm-publish-nexus.go
@@ -36,10 +36,6 @@ func (action PublishActionStruct) Check(ctx api.ActionExecutionContext) bool {
 		missingRequirements = append(missingRequirements, api.MissingRequirement{Message: "no module context present"})
 	}
 
-	for _, r := range missingRequirements {
-		log.Warn().Str("condition", r.Message).Msg("action requirements not fulfilled")
-	}
-
 	return len(missingRequirements) == 0
 }
 
@@ -61,10 +57,10 @@ func (action PublishActionStruct) Execute(ctx api.ActionExecutionContext, state 
 		}
 
 		for _, file := range files {
-			endpoint := nexusUrl+"/service/rest/v1/components?repository="+nexusRepo
+			endpoint := nexusUrl + "/service/rest/v1/components?repository=" + nexusRepo
 			log.Info().Str("nexus", endpoint).Str("chart", file).Msg("uploading chart to nexus")
 			status, response := UploadChart(endpoint, nexusUsername, nexusPassword, file)
-			if (status < 200 || status >= 300) {
+			if status < 200 || status >= 300 {
 				log.Warn().Str("chart", file).Int("status", status).Str("response", string(response)).Msg("chart upload failed")
 			}
 		}
