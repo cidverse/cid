fc81955a2d485749c62eb4816e33c2160eed0d38
Philipp Heuer <git@philippheuer.me>
2023-05-10 23:30:29 +0200

    feat: filter base64 decoded values from output

--- pkg/common/protectoutput/protect.go
+++ pkg/common/protectoutput/protect.go
@@ -11,9 +11,21 @@ var protectedPhrases []string
 
 // ProtectPhrase will cause the provided phrase to be redacted (also base64 encoded values)
 func ProtectPhrase(phrase string) {
+	if phrase == "" {
+		return
+	}
+
 	if !funk.Contains(protectedPhrases, phrase) {
 		protectedPhrases = append(protectedPhrases, phrase)
 
+		// add base64 decoded version, if the phrase is base64 encoded
+		if isBase64(phrase) {
+			// add base64 decoded version, if the phrase is base64 encoded
+			decodedValue, _ := base64.StdEncoding.DecodeString(phrase)
+			protectedPhrases = append(protectedPhrases, string(decodedValue))
+		}
+
+		// add base64 encoded version of the phrase
 		phraseBase64 := base64.StdEncoding.EncodeToString([]byte(phrase))
 		protectedPhrases = append(protectedPhrases, phraseBase64)
 	}
@@ -26,4 +38,9 @@ func RedactProtectedPhrases(input string) string {
 	}
 
 	return input
+}
+
+func isBase64(s string) bool {
+	_, err := base64.StdEncoding.DecodeString(s)
+	return err == nil
 }
--- pkg/common/protectoutput/protect_test.go
+++ pkg/common/protectoutput/protect_test.go
@@ -10,10 +10,11 @@ func TestPhraseAddition(t *testing.T) {
 	protectedPhrases = nil
 
 	// protect phrase
-	ProtectPhrase("mysecret")
-	assert.Equal(t, 2, len(protectedPhrases))
-	assert.Equal(t, "mysecret", protectedPhrases[0])
-	assert.Equal(t, "bXlzZWNyZXQ=", protectedPhrases[1])
+	ProtectPhrase("bXlzZWNyZXQ=")
+	assert.Equal(t, 3, len(protectedPhrases))
+	assert.Equal(t, "bXlzZWNyZXQ=", protectedPhrases[0])
+	assert.Equal(t, "mysecret", protectedPhrases[1])
+	assert.Equal(t, "YlhselpXTnlaWFE9", protectedPhrases[2])
 }
 
 func TestRedaction(t *testing.T) {
@@ -36,4 +37,15 @@ func TestRedactionBase64(t *testing.T) {
 	// check redacted
 	out := RedactProtectedPhrases("abc bXlzZWNyZXQ= def")
 	assert.Equal(t, "abc [MASKED] def", out)
+}
+
+func TestRedactionBase64Encoded(t *testing.T) {
+	protectedPhrases = nil
+
+	// protect phrase
+	ProtectPhrase("bXlzZWNyZXQ=")
+
+	// check redacted
+	out := RedactProtectedPhrases("test mysecret test")
+	assert.Equal(t, "test [MASKED] test", out)
 }
