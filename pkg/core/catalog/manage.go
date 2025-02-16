07144958355aa3b3f0cb8c3a4b14d11e5a6c158c
Philipp Heuer <git@philippheuer.de>
2025-02-16 21:46:00 +0100

    chore: use develop catalog as default value for now

--- pkg/core/catalog/manage.go
+++ pkg/core/catalog/manage.go
@@ -35,7 +35,8 @@ func LoadSources() map[string]*Source {
 
 	// file doesn't exist yet, init with main repo
 	if _, err := os.Stat(repositoryConfigFile); os.IsNotExist(err) {
-		sources["cid"] = &Source{URI: "https://raw.githubusercontent.com/cidverse/catalog/main/cid-index.yaml", AddedAt: time.Now().Format(time.RFC3339), UpdatedAt: time.Now().Format(time.RFC3339), SHA256: ""}
+		sources["cid"] = &Source{URI: "https://raw.githubusercontent.com/cidverse/catalog/develop/cid-index.json", AddedAt: time.Now().Format(time.RFC3339), UpdatedAt: time.Now().Format(time.RFC3339), SHA256: ""}
+		sources["cid-actions"] = &Source{URI: "oci://quay.io/cidverse/cid-actions-go:0.1.0", AddedAt: time.Now().Format(time.RFC3339), UpdatedAt: time.Now().Format(time.RFC3339), SHA256: ""}
 		return sources
 	}
 
--- pkg/util/directory-setup.go
+++ pkg/util/directory-setup.go
@@ -3,6 +3,7 @@ package util
 import (
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/rs/zerolog/log"
 )
@@ -14,6 +15,11 @@ func DirectorySetup() {
 }
 
 func requireDirectory(dir string) {
+	// nixos build sandbox, no need to create directories
+	if strings.HasPrefix(dir, "/homeless-shelter") {
+		return
+	}
+
 	err := os.MkdirAll(dir, os.ModePerm)
 	if err != nil {
 		log.Fatal().Err(err).Str("dir", dir).Msg("failed to create required directory")
