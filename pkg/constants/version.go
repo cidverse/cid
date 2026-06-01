916b25493192a80c07c06b6e1801f9b854e75711
Philipp Heuer <git@philippheuer.de>
2026-06-01 21:33:00 +0200

    chore: bump version to v0.11.4

--- pkg/constants/version.go
+++ pkg/constants/version.go
@@ -3,7 +3,7 @@ package constants
 import "github.com/cidverse/cid/pkg/util"
 
 var (
-	Version          = "0.11.3"
+	Version          = "0.11.4"
 	CommitHash       = "none"
 	BuildAt          = "unknown"
 	RepositoryStatus = "clean"
